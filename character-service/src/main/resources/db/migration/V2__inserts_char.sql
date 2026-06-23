6769b36050e35d6a592d0a80af6713bd6131cb65
Rorrop24 <ro.catalani@duocuc.cl>
2026-06-23 17:54:50 -0400

    Se arregla las bases de datos y se añade el  jar para los dockers

--- character-service/src/main/resources/db/migration/V2__inserts_char.sql
+++ character-service/src/main/resources/db/migration/V2__inserts_char.sql
@@ -3,6 +3,8 @@ INSERT INTO personajes
 VALUES
     ('León', 'Humano', 'Hechicero', 20, 350,
      'Portador del fuego de destrucción y creación. Sobreviviente de una tragedia olvidada.', TRUE),
+    ('Mira', 'Elfa', 'Exploradora', 5, 42,
+     'Arquera viajera vinculada a las primeras campanas de DragonForge.', TRUE)
 ;
 INSERT INTO equipamientos
 (nombre, tipo, cantidad, personaje_id)
